1cd689105c1b941267c4c25a9ae0ade7ac5539ad
PhuNguyenUET <phunguyen.it.k67@gmail.com>
2024-05-17 09:03:06 +0700

    Pre-merge

--- lib/screens/HomeScreen/Home.dart
+++ lib/screens/HomeScreen/Home.dart
@@ -31,166 +31,179 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: HexColor('#B0B0B0'),
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "Hey, ",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.normal),
-                ),
-                Text(
-                  "Sky!",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  width: 7,
-                ),
-                Icon(
-                  Symbols.hand_gesture,
-                  size: 25,
-                  weight: 300,
-                ),
-                Expanded(
-                    child: SizedBox(
-                  width: double.infinity,
-                )),
-                CalendarBox(day: "Sun, 4 Jun"),
-                SizedBox(
-                  width: 7,
-                ),
-                StreakBox(streakCount: 5),
-                SizedBox(
-                  width: 15,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      height: 120,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: AnimationLimiter(
-                              child: ListView.builder(
-                                itemCount: testDayList.length,
-                                cacheExtent: 1000,
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return AnimationConfiguration.staggeredList(
-                                    position: index,
-                                    duration: const Duration(milliseconds: 700),
-                                    child: SlideAnimation(
-                                      horizontalOffset: 100.0,
-                                      child: FadeInAnimation(
-                                          child: Row(
-                                        children: [
-                                          SizedBox(
-                                            width: 12,
-                                          ),
-                                          DayTile(
-                                            moodOfDay: MoodOfDay(
-                                            dayOfMonth: testDayList[index],
-                                            dayOfWeek: testDayOfWeekList[index],
-                                            isToday: index == testToday,
-                                            moodId: testMoodList[index]),
-                                          ),
-                                        ],
-                                      )),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 15,
-                          ),
-                        ],
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                //TODO: Experiment more
+                Colors.white,
+                HexColor('ffcccc'),
+                HexColor('ffd9cc'),
+              ]
+            )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Hey, ",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                    "Sky!",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 7,
+                  ),
+                  Icon(
+                    Symbols.hand_gesture,
+                    size: 25,
+                    weight: 300,
+                  ),
+                  Expanded(
+                      child: SizedBox(
+                    width: double.infinity,
+                  )),
+                  CalendarBox(day: "Sun, 4 Jun"),
+                  SizedBox(
+                    width: 7,
+                  ),
+                  StreakBox(streakCount: 5),
+                  SizedBox(
+                    width: 15,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25.0),
-                            color: HexColor("#DED7FA"),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text(
-                                    "Mood chart",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 10),
-                              Container(
-                                  height: 300,
-                                  child: MoodChart(moods: SampleData.moods,)),
-                            ],
-                          )),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: 5,
-                      cacheExtent: 1000,
-                      scrollDirection: Axis.vertical,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Column(
+                      SizedBox(
+                        height: 120,
+                        child: Row(
                           children: [
-                            MoodArticle(
-                              mood: SampleData.moods[index]
+                            Expanded(
+                              child: AnimationLimiter(
+                                child: ListView.builder(
+                                  itemCount: testDayList.length,
+                                  cacheExtent: 1000,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    return AnimationConfiguration.staggeredList(
+                                      position: index,
+                                      duration: const Duration(milliseconds: 700),
+                                      child: SlideAnimation(
+                                        horizontalOffset: 100.0,
+                                        child: FadeInAnimation(
+                                            child: Row(
+                                          children: [
+                                            SizedBox(
+                                              width: 12,
+                                            ),
+                                            DayTile(
+                                              moodOfDay: MoodOfDay(
+                                              dayOfMonth: testDayList[index],
+                                              dayOfWeek: testDayOfWeekList[index],
+                                              isToday: index == testToday,
+                                              moodId: testMoodList[index]),
+                                            ),
+                                          ],
+                                        )),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
                             ),
-                            SizedBox(height: 10,),
+                            SizedBox(
+                              width: 15,
+                            ),
                           ],
-                        );
-                      },
-                    ),
-                  ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25.0),
+                              color: HexColor("#DED7FA"),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      "Mood chart",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 10),
+                                Container(
+                                    height: 300,
+                                    child: MoodChart(moods: SampleData.moods,)),
+                              ],
+                            )),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: 5,
+                        cacheExtent: 1000,
+                        scrollDirection: Axis.vertical,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
+                            children: [
+                              MoodArticle(
+                                mood: SampleData.moods[index]
+                              ),
+                              SizedBox(height: 10,),
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
